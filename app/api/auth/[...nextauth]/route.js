48bb97f9d74ffe7e51880ef5d5dacbbe4271d443
ahmmedrasel-dev <rahmmed.info@gmail.com>
2023-05-16 18:05:34 +0600

    update routes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -16,6 +16,9 @@ const handler = NextAuth({
         const sessionUser = await User.findOne({
             email: session.user.email, 
         })
+
+        session.user.id = sessionUser._id.toString();
+        return session;
     },
     async signIn({profile}){
         try{
