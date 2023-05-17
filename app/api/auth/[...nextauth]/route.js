4ea6219a47589a68b540abfee57ea88b1a53394c
Ahmmed Rasel <rahmmed.info@gmail.com>
2023-05-17 10:48:14 +0600

    serach added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,48 +4,48 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
 
- 
+
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
-    ],
-    callbacks: {
-        
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      })
+
+      session.user.id = sessionUser._id.toString();
+      return session;
     },
-    async session({session}){
-        const sessionUser = await User.findOne({
-            email: session.user.email, 
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+        // Check User Already Exists;
+        const userExists = await User.findOne({
+          email: profile.email,
         })
 
-        session.user.id = sessionUser._id.toString();
-        return session;
-        },
-        async signIn({profile}){
-            try{
-                await connectToDB();
-                // Check User Already Exists;
-                const userExists = await User.findOne({
-                    email: profile.email,
-                })
-
-                // If Not Create New User;
-                if(!userExists){
-                    await User.create({
-                        email: profile.email,
-                        username: profile.name.replace(" ", "").toLowerCase(),
-                        image: profile.picture,
-                    })
-                }
-                return true;
-            }
-            catch(error){
-                console.log(error);
-                return false;
-            }
+        // If Not Create New User;
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          })
         }
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
+  },
+
 })
 
-export { handler as GET, handler as POST};
+export { handler as GET, handler as POST };
--- utils/database.js
+++ utils/database.js
@@ -1,25 +1,23 @@
 import mongoose from "mongoose";
 let isConnect = false // Track the connection;
-const connectToDB = async ( ) => {
-    mongoose.set('strictQuery', true);
-    if(isConnect){
-        console.log('Mongodb is already connected!');
-        return;
-    }
+const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+  if (isConnect) {
+    console.log('Mongodb is already connected!');
+    return;
+  }
 
-    try{
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: 'prompt_genai',
-            useNewUrlParse: true,
-            useUnifiedTopology: true,
-        })
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'prompt_genai',
+    })
 
-        isConnect = true;
-        console.log('Mongodb Connected!');
-    }
-    catch(error){
-        console.log(error.message);
-    }
+    isConnect = true;
+    console.log('Mongodb Connected!');
+  }
+  catch (error) {
+    console.log(error.message);
+  }
 }
 
 export default connectToDB;
